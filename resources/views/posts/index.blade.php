65a94778f20cc71170cfb020b7f40ec27039c4e4
joel <joel.uss1728@gmail.com>
2022-11-15 09:29:11 -0700

    mejoras visuales

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,31 +7,29 @@
 
 
             @foreach($posts as $post)
+                <x-card-post :post=$post/>
         
-                <article  class="w-full h-80 bg-cover bg-center @if($loop->first) md:col-span-2 @endif" style="background-image: url(@if($post->image)'{{Storage::url($post->image->url)}}'@else 'https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg' @endif)">
+{{--                 <article  class="w-full h-80 bg-cover bg-center @if($loop->first) md:col-span-2 @endif" style="background-image: url(@if($post->image)'{{Storage::url($post->image->url)}}'@else 'https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg' @endif)">
         
 {{--   <article  class="w-full h-80 bg-cover bg-center @if($loop->first) md:col-span-2 @endif" style="background-image: url('https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179__480.jpg')">
-  --}}
+  --C}}
              <div class="w-full h-full px-8 flex flex-col justify-center">
                         <div>
                             @foreach($post->tags as $tag)
                                 <a href="{{route('post.tag', $tag)}}"class="inline-blog px-3 h-6 bg-{{$tag->color}}-600 text-white rounded-full">{{$tag->name}}</a>
                             @endforeach
                         </div>
-                        <h1 class="mt-2 text-4xl text-white leading-8 font-bold">
+                        <h1 class="mt-2 text-4xl leading-8 font-bold">
                             <a href="{{route('posts.show', $post)}}">
                                 {{$post->name}}
                             </a>
                         </h1>
                     </div>     
-                </article>
+                </article> --}}
             @endforeach
         </div>
         <div class="mb-4">
             {{$posts->links()}}
         </div>
     </div>
-
-
-
 </x-app-layout>
